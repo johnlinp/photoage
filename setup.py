d4765ca7752d20f0839f59154406561911113a1e
John Lin <johnlinp@gmail.com>
2020-07-18 07:30:46 +0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='photoage',
-    version='0.1.0',
+    version='0.1.1',
     description='Calculate the age of a photo.',
     long_description=long_description,
     long_description_content_type='text/markdown',
